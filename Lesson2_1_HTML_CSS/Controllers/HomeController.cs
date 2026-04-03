5250c1129ad3ee5de3efa6c6df29e09f14629a8d
resulturfent <rsltrfnt@gmail.com>
2026-04-03 21:40:21 +0300

    css tag,class,id işlemleri

---
+++ Lesson2_1_HTML_CSS/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using Lesson2_1_HTML_CSS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lesson2_1_HTML_CSS.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Lesson2_1_HTML_CSS/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Lesson2_1_HTML_CSS.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
