7871ebef89aa1ec5c7e43687789fed4fa4e4cc08
Murat ÇIPLAK <50632672+muratapk@users.noreply.github.com>
2026-04-09 20:40:06 +0300

    javasccritp için değişkenlerl, function işlemi yapıldı

---
+++ Lesson4_1_javascript_giris/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lesson4_1_javascript_giris.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
