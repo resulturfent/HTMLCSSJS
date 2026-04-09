0dfca19ffbe8031c6914c052ee3d8e6006e1ec2c
Murat ÇIPLAK <50632672+muratapk@users.noreply.github.com>
2026-04-09 21:35:08 +0300

    javascript parseInt(), örnek çözümü

---
+++ Lesson4_2_javascript_ornek/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lesson4_2_javascript_ornek.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
